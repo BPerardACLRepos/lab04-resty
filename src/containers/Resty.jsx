2fb4bc84f62b2ac72cf8cafd700c7c7232bcea3a
Brandon Perard <brandon.perard@gmail>
2021-05-17 12:50:44 -0700

    add Resty.jsx container with state outline

---
+++ src/containers/Resty.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+export default class Resty extends Component {
+    state = {
+        loading: false,
+        url: '',
+        method: '',
+        body: '',
+        response: '',
+        history: '',
+    }
+
+
+    render() {
+        return (
+            <>
+            </>
+        );
+    }
+}
